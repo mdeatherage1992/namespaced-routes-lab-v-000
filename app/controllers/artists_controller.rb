8cd69f059a85b8201ca6d0c67047b14dc22894c0
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-15 14:48:43 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,11 @@
 class ArtistsController < ApplicationController
    before_action :set_preferences, only: [:index, :new]
   def index
-    @artists = Artist.all
+    if @preferences && @preferences.artist_sort_order
+      @artists = Artist.order(name: @preferences.artist_sort_order)
+    else
+      @artists = Artist.all
+    end
   end
 
   def show
